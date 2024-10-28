3b9dd9f276876d8d47f4de1cadd88e549cac52dd
ttpweb <randomnig@proton.me>
2024-10-28 23:29:32 +0300

    jsreplace.js

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-// Metindeki tüm 'a' harflerini 'b' ile değiştiren fonksiyon
-function replaceAWithB(text) {
-    return text.replace(/a/g, 'b');  // Tüm 'a' harflerini 'b' ile değiştirir
-}
 
-// Input alıp sonucu HTML'de gösteren işlev
-function handleTextChange() {
-    const inputText = document.getElementById("inputText").value;  // Kullanıcıdan metni alır
-    const resultText = replaceAWithB(inputText);  // Metindeki 'a' harflerini 'b' ile değiştirir
-    document.getElementById("outputText").innerText = resultText;  // Sonucu ekrana yazar
-}
